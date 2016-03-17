9aad9cd773ae4ef9d1fbd7443ca040ef7fe9fb03
Ardeny <xie2001012@gmail.com>
2016-03-17 14:51:32 +0800

    add i3lock.sh

--- i3lock.sh
+++ i3lock.sh
@@ -1,1 +1,20 @@
-/home/alan/bin/i3lock.sh
+
+# File Name: /home/alan/bin/i3lock.sh
+# Author: Alan Tse
+# mail:xie2001012@gmail.com
+# Created Time: 2016年02月29日 星期一 15时11分19秒
+#########################################################################
+#!/bin/sh -e
+
+# Take a screenshot
+scrot /home/alan/Pictures/i3lock_pic.png
+
+#Pixellate it 10x
+mogrify -scale 10% -scale 1000% /home/alan/Pictures/i3lock_pic.png
+
+#Lock screen displaying this image.
+i3lock -i /home/alan/Pictures/i3lock_pic.png
+
+# Turn the screen off after a delay.
+sleep 60; pgrep i3lock && xset dpms force off
+
